638f27be38b984045494677a9b9ce137e89fceb3
igenena <43812305+igenena@users.noreply.github.com>
2021-06-25 14:33:10 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App {
   public static void main(String[] args) {
     System.out.println(new App().getMessage());
   }
-
+///hjhgj
   private final String getMessage() {
     return message;
   }
